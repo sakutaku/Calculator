7c789242bcb8d7c9d0554caf1b46b5a8da15edbd
Abytova Saikal <saikalabytova1@gmail.com>
2023-07-12 13:26:16 +0600

    Add some new styles

--- src/containers/Calculator/Calculator.tsx
+++ src/containers/Calculator/Calculator.tsx
@@ -14,15 +14,15 @@ const Calculator = () => {
                 <div className="calculator-nums" onClick={() => dispatch(press('7'))}>7</div>
                 <div className="calculator-nums" onClick={() => dispatch(press('8'))}>8</div>
                 <div className="calculator-nums" onClick={() => dispatch(press('9'))}>9</div>
-                <div className="calculator-nums" onClick={() => dispatch(calcValue('%'))}>%</div>
+                <div className="calculator-nums calculator-math" onClick={() => dispatch(calcValue('%'))}>%</div>
                 <div className="calculator-nums" onClick={() => dispatch(press('4'))}>4</div>
                 <div className="calculator-nums" onClick={() => dispatch(press('5'))}>5</div>
                 <div className="calculator-nums" onClick={() => dispatch(press('6'))}>6</div>
-                <div className="calculator-nums" onClick={() => dispatch(calcValue('*'))}>*</div>
+                <div className="calculator-nums calculator-math" onClick={() => dispatch(calcValue('*'))}>*</div>
                 <div className="calculator-nums" onClick={() => dispatch(press('1'))}>1</div>
                 <div className="calculator-nums" onClick={() => dispatch(press('2'))}>2</div>
                 <div className="calculator-nums" onClick={() => dispatch(press('3'))}>3</div>
-                <div className="calculator-nums" onClick={() => dispatch(calcValue('-'))}>-</div>
+                <div className="calculator-nums calculator-math" onClick={() => dispatch(calcValue('-'))}>-</div>
                 <div className="calculator-nums-empty">
                     <div className="calculator-delete" onClick={() => dispatch(deleteValue())}>{"<"}</div>
                     <div className="calculator-ac" onClick={() => dispatch(resetValue())}>{"AC"}</div>
@@ -31,7 +31,7 @@ const Calculator = () => {
                 <div className="calculator-nums-empty">
                     <div className="calculator-check" onClick={() => dispatch(checkValue())}>E</div>
                 </div>
-                <div className="calculator-nums" onClick={() => dispatch(calcValue('+'))}>+</div>
+                <div className="calculator-nums calculator-math" onClick={() => dispatch(calcValue('+'))}>+</div>
             </div>
         </div>
     );
